30b81f2dc5d1a241fa1a0871f2b0498c8ef82943
Ohdaejin <xla3095@gmail.com>
2022-02-28 12:00:55 +0900

    회원가입 완료 후, 생성된 회원 보여줌

--- src/main/java/com/odj/exam/demo/controller/UsrMemberController.java
+++ src/main/java/com/odj/exam/demo/controller/UsrMemberController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.odj.exam.demo.service.MemberService;
+import com.odj.exam.demo.vo.Member;
+
 @Controller
 public class UsrMemberController {
 	private MemberService memberService;
@@ -15,9 +17,12 @@ public class UsrMemberController {
 
 	@RequestMapping("/usr/member/doJoin")
 	@ResponseBody
-	public String doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNo,
+	public Member doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNo,
 			String email) {
-		memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
-		return "성공";
+		int id = memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
+		
+		Member member = memberService.getMemberById(id);
+		
+		return member;
 	}
 }
--- src/main/java/com/odj/exam/demo/service/MemberService.java
+++ src/main/java/com/odj/exam/demo/service/MemberService.java
@@ -3,16 +3,22 @@ package com.odj.exam.demo.service;
 import org.springframework.stereotype.Service;
 
 import com.odj.exam.demo.repository.MemberRepository;
+import com.odj.exam.demo.vo.Member;
 
 @Service
 public class MemberService {
 	private MemberRepository memberRepository;
-	
+
 	public MemberService(MemberRepository memberRepository) {
 		this.memberRepository = memberRepository;
 	}
 
-	public void join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
+	public int join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
 		memberRepository.join(loginId, loginPw, name, nickname, cellphoneNo, email);
+		return memberRepository.getLastInsertId();
+	}
+
+	public Member getMemberById(int id) {
+		return memberRepository.getMemberById(id);
 	}
 }
