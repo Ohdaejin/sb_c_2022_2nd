1272ef04fdfe2cf787cffa947f22ff193e489225
Ohdaejin <xla3095@gmail.com>
2022-03-11 11:43:11 +0900

    데이지UI적용

--- src/main/java/com/odj/exam/demo/vo/Article.java
+++ src/main/java/com/odj/exam/demo/vo/Article.java
@@ -10,11 +10,20 @@ import lombok.NoArgsConstructor;
 public class Article {
 	private int id;
 	private String regDate;
-	private String updateDate;	
+	private String updateDate;
 	private int memberId;
 	private String title;
 	private String body;
-
+	
 	private String extra__writerName;
+	private boolean extra__actorCanModify;
 	private boolean extra__actorCanDelete;
+	
+	public String getRegDateForPrint() {
+		return regDate.substring(2, 16);
+	}
+	
+	public String getUpdateDateForPrint() {
+		return updateDate.substring(2, 16);
+	}
 }
