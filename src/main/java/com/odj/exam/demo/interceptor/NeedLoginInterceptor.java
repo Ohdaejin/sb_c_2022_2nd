09dca0a9c9c66ea3519bc80bb459320fb1d5df0d
Ohdaejin <xla3095@gmail.com>
2022-03-11 10:14:59 +0900

    NeedLogin인터셉터 구현 및 등록

---
+++ src/main/java/com/odj/exam/demo/interceptor/NeedLoginInterceptor.java
@@ -0,0 +1,19 @@
+package com.odj.exam.demo.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import com.odj.exam.demo.vo.Rq;
+
+@Component
+public class NeedLoginInterceptor implements HandlerInterceptor {
+	@Override
+	public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws Exception {
+		System.out.println("로그인 필요!!");
+
+		return HandlerInterceptor.super.preHandle(req, resp, handler);
+	}
+}
