8058f614b1215e940a0e1d0f7cb846c5515ad194
Ohdaejin <xla3095@gmail.com>
2022-03-31 09:36:40 +0900

    처음부터 rq를 무조건 만드는 방식을 수정, 필요할 때만 만듬

--- src/main/java/com/odj/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/odj/exam/demo/controller/UsrHomeController.java
@@ -2,9 +2,6 @@ package com.odj.exam.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.odj.exam.demo.service.MemberService;
 
 @Controller
 public class UsrHomeController {
